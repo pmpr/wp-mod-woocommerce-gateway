ba523c4e524778c8a6b9dd90d9f87157ab12e798
Pmnmpr <pmnmpr@gmail.com>
2021-09-26 10:17:57 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             615048ae7f5fd             |
+    |_______________________________________|
+*/
+ use Pmpr\Package\WoocommerceGateway\WoocommerceGateway; WoocommerceGateway::symcgieuakksimmu();
