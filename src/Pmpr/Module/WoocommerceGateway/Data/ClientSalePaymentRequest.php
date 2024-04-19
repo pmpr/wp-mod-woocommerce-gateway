7b103e5b1a097f5f8ee8db3f5470ca9c31ca02cc
pmpr.dev <pmpr.dev@gmail.com>
2024-04-19 08:49:10 +0000

    Obfuscated

--- src/Pmpr/Module/WoocommerceGateway/Data/ClientSalePaymentRequest.php
+++ src/Pmpr/Module/WoocommerceGateway/Data/ClientSalePaymentRequest.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66222f12c7b7a             |
+    |             66222fe08605d             |
     |_______________________________________|
 */
  namespace Pmpr\Module\WoocommerceGateway\Data; class ClientSalePaymentRequest { public $Amount; public $OrderId; public $CallBackUrl; public $LoginAccount; public $AdditionalData; public function qiccuiwooiquycsg(array $yiosiwewiecqmkaa) { foreach ($yiosiwewiecqmkaa as $csgiecsagosuucqo => $eqgoocgaqwqcimie) { if (!isset($this->{$csgiecsagosuucqo})) { goto kqqiegkuqagcqsya; } $this->{$csgiecsagosuucqo} = $eqgoocgaqwqcimie; kqqiegkuqagcqsya: miweggwqeiaeweia: } kkumywowcoycymeo: } }
--- src/Pmpr/Module/WoocommerceGateway/Saderat.php
+++ src/Pmpr/Module/WoocommerceGateway/Saderat.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66222f12c7b7a             |
+    |             66222fe08605d             |
     |_______________________________________|
 */
  namespace Pmpr\Module\WoocommerceGateway; use nusoap_client; use Pmpr\Common\Foundation\Interfaces\IconInterface; class Saderat extends Gateway { protected function ussowkigumoaoowo($umwqusowiqmyseom) { } protected function aqmwamyiwgeeymqa($umwqusowiqmyseom) { } protected function qyeykswoowmwqmai() { } }
