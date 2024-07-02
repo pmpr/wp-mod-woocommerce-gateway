959750aac2f921c6e9ec516417ac65efb87acc5b
pmpr.dev <pmpr.dev@gmail.com>
2024-07-02 13:32:16 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6646a8e10fe4b             |
+    |             6684013e9c23d             |
     |_______________________________________|
 */
  use Pmpr\Module\WoocommerceGateway\WoocommerceGateway; WoocommerceGateway::symcgieuakksimmu();
--- src/Pmpr/Module/WoocommerceGateway/Data/ClientSalePaymentRequest.php
+++ src/Pmpr/Module/WoocommerceGateway/Data/ClientSalePaymentRequest.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6646a8e10fe4b             |
+    |             6684013e9c23d             |
     |_______________________________________|
 */
- namespace Pmpr\Module\WoocommerceGateway\Data; class ClientSalePaymentRequest { public $Amount; public $OrderId; public $CallBackUrl; public $LoginAccount; public $AdditionalData; public function qiccuiwooiquycsg(array $yiosiwewiecqmkaa) { foreach ($yiosiwewiecqmkaa as $csgiecsagosuucqo => $eqgoocgaqwqcimie) { if (!isset($this->{$csgiecsagosuucqo})) { goto foeeqckqsyockkak; } $this->{$csgiecsagosuucqo} = $eqgoocgaqwqcimie; foeeqckqsyockkak: oqugqwcyomiaaoqu: } eeqesooyqagwawae: } }
+ namespace Pmpr\Module\WoocommerceGateway\Data; class ClientSalePaymentRequest { public $Amount; public $OrderId; public $CallBackUrl; public $LoginAccount; public $AdditionalData; public function qiccuiwooiquycsg(array $yiosiwewiecqmkaa) { foreach ($yiosiwewiecqmkaa as $csgiecsagosuucqo => $eqgoocgaqwqcimie) { if (!isset($this->{$csgiecsagosuucqo})) { goto eegqyykygiccaoeo; } $this->{$csgiecsagosuucqo} = $eqgoocgaqwqcimie; eegqyykygiccaoeo: miyqyeiwquwsacsm: } qkcyqocqqwmqgqww: } }
